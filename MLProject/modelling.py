061b71eb44e6af3b5cfaf129177877b8b150b67b
adityaglory <gloryadityaj@gmail.com>
2026-06-01 17:05:53 +0700

    Solve mlflow CLI

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -9,7 +9,7 @@ def train_model():
     train_path = os.path.join(BASE_DIR, 'bank_churn_preprocessing', 'train.csv')
     test_path = os.path.join(BASE_DIR, 'bank_churn_preprocessing', 'test.csv')
     
-    print("Loading training and test data...")
+    print("Loading data...")
     train_data = pd.read_csv(train_path)
     test_data = pd.read_csv(test_path)
     
@@ -17,6 +17,9 @@ def train_model():
     y_train = train_data['Exited']
     X_test = test_data.drop('Exited', axis=1)
     y_test = test_data['Exited']
+
+    os.environ["MLFLOW_ALLOW_FILE_STORE"] = "true"
+    mlflow.set_tracking_uri("file:./mlruns")
 
     with mlflow.start_run() as run:
         rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
@@ -27,9 +30,7 @@ def train_model():
         
         mlflow.sklearn.log_model(rf_model, "model")
         
-        workspace_dir = os.environ.get("GITHUB_WORKSPACE", ".")
-        run_id_path = os.path.join(workspace_dir, "MLProject", "run_id.txt")
-        with open(run_id_path, "w") as f:
+        with open("run_id.txt", "w") as f:
             f.write(run.info.run_id)
 
 if __name__ == "__main__":
