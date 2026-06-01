be58af52849630726fd98d9e959e6766d5bb1e8f
adityaglory <gloryadityaj@gmail.com>
2026-06-01 16:47:54 +0700

    Remove set_experiment to fix MLflow ID conflict

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -9,6 +9,7 @@ def train_model():
     train_path = os.path.join(BASE_DIR, 'bank_churn_preprocessing', 'train.csv')
     test_path = os.path.join(BASE_DIR, 'bank_churn_preprocessing', 'test.csv')
     
+    print("Loading training and test data...")
     train_data = pd.read_csv(train_path)
     test_data = pd.read_csv(test_path)
     
@@ -17,11 +18,9 @@ def train_model():
     X_test = test_data.drop('Exited', axis=1)
     y_test = test_data['Exited']
 
-    mlflow.set_experiment("Bank_Churn_Prediction")
-
     mlflow.sklearn.autolog()
 
-    with mlflow.start_run(run_name="CI_Pipeline_Run") as run:
+    with mlflow.start_run() as run:
         rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
         rf_model.fit(X_train, y_train)
         
