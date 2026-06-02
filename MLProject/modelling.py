566452e0fa6c9368ddfd85d10887b6bfc577a89f
adityaglory <gloryadityaj@gmail.com>
2026-06-02 08:24:51 +0700

    fix mlflow

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -18,9 +18,6 @@ def train_model():
     X_test = test_data.drop('Exited', axis=1)
     y_test = test_data['Exited']
 
-    os.environ["MLFLOW_ALLOW_FILE_STORE"] = "true"
-    mlflow.set_tracking_uri("file:./mlruns")
-
     with mlflow.start_run() as run:
         rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
         rf_model.fit(X_train, y_train)
